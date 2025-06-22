dbe0f3006467e09d0617e1d12ab48ae36d267005
Nicholas Musolino <n.musolino@gmail.com>
2025-06-22 12:18:25 -0400

    Add docstrings

--- src/miter/__init__.py
+++ src/miter/__init__.py
@@ -7,6 +7,12 @@ from miter._typing import Predicate
 T = typing.TypeVar("T")
 
 def count(iterable: Iterable) -> int:
+    """
+    Return the number of elements in `iterable`, by calling `len()` or
+    by iterating and counting elements.
+
+    Complexity: `O(1)` if `len(iterable)` succeeds, `O(N)` otherwise.
+    """
     try:
         return len(cast(Sequence, iterable))
     except TypeError:
@@ -14,4 +20,11 @@ def count(iterable: Iterable) -> int:
     return _core.count(iterable)
 
 def count_if(iter: Iterable[T], pred: Predicate[T]) -> int:
+    """
+    Return the number of elements in `iterable` for which `pred`
+    is true.
+
+    Complexity: `O(N)` applications of the predicate function.
+    """
+
     return _core.count(filter(pred, iter))
