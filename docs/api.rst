b875fcf622eb5287d5be4aca270a4750d5d87d66
Nicholas Musolino <n.musolino@gmail.com>
2025-06-22 12:13:07 -0400

    Add documentation generation

---
+++ docs/api.rst
@@ -0,0 +1,14 @@
+===================
+Miter API Reference
+===================
+
+This page documents the functions and classes within ``miter``.
+
+.. currentmodule:: miter
+
+Utilities for Iterables
+=======================
+
+.. autofunction:: count
+
+.. autofunction:: count_if
---
+++ docs/conf.py
@@ -0,0 +1,66 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from __future__ import annotations
+
+# Warning: do not change the path here. To use autodoc, you need to install the
+# package first.
+
+# -- Project information -----------------------------------------------------
+
+project = "miter"
+copyright = "2022, Nicholas Musolino"
+author = "Nicholas Musolino"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "python_docs_theme"
+    # "sphinx.ext.napoleon",
+    # "sphinx_copybutton",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = []
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "**.ipynb_checkpoints", "Thumbs.db", ".DS_Store", ".env"]
+
+# Whether to add module names to function names, as in `miter.all_equal()`.
+add_module_names = True
+# Whether to strip module names from type annotations (?).
+python_use_unqualified_type_names = True
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "python_docs_theme"
+
+html_title = f"{project}"
+
+html_baseurl = "https://miter.readthedocs.io/en/latest/"
+
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_sidebars
+html_sidebars = {
+   '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path: list[str] = []
